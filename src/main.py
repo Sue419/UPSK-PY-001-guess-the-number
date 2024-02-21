ff34368babcdf4055fe0481d2e22a87240fef65a
Sue419 <suzan419@gmail.com>
2024-02-20 23:32:32 -0500

    FEAT: Counting player and computer guesses

--- src/main.py
+++ src/main.py
@@ -24,10 +24,15 @@ def guess_the_number():
     random_number = generate_random_number()
     print("¡Adivina el número!")
 
+    player_guesses = 0
+    computer_guesses = 0
+
     while True:
         player_number = int(input('Ingrese un número: '))
         player_message, player_success = human_guess(player_number, random_number)
         print(player_message)
+
+        player_guesses += 1
 
         if player_success:
             break
@@ -36,8 +41,14 @@ def guess_the_number():
         computer_message, computer_success = computer_guess(computer_number, random_number)
         print(computer_message)
 
+        computer_guesses += 1
+
         if computer_success:
             break
+    
+    #Número de intentos
+    print(f"Cantidad de intentos realizados por el jugador: {player_guesses}")
+    print(f"Cantidad de intentos del computador: {computer_guesses}")
 
     play_again = input('¿Quieres jugar de nuevo? (si/no)? ')
     return play_again.lower() == 'si'
