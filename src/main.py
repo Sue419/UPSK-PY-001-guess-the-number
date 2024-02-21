22264721155d8a335d2fe8fdf065eade9225c726
Sue419 <suzan419@gmail.com>
2024-02-20 23:22:54 -0500

    FIX(logic_game): Correct execution of test without needing to play first

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ def computer_guess(computer_number, random_number):
         return f'El ordenador adivina: {computer_number}. El número es muy bajo', False
 
 def guess_the_number():
+
     random_number = generate_random_number()
     print("¡Adivina el número!")
 
@@ -27,24 +28,28 @@ def guess_the_number():
         player_number = int(input('Ingrese un número: '))
         player_message, player_success = human_guess(player_number, random_number)
         print(player_message)
+
         if player_success:
             break
 
         computer_number = random.randint(1, 100)
         computer_message, computer_success = computer_guess(computer_number, random_number)
         print(computer_message)
+
         if computer_success:
             break
 
     play_again = input('¿Quieres jugar de nuevo? (si/no)? ')
     return play_again.lower() == 'si'
 
-while True:
-  # Jugar una partida
-  play_again = guess_the_number()
+#Para ejecutar el juego
+if __name__ == "__main__":
+  while True:
+    # Jugar una partida
+    play_again = guess_the_number()
 
-  # Preguntar si quiere jugar de nuevo
-  if not play_again:
-    break
+    # Salir del bucle si el usuario no quiere jugar de nuevo
+    if not play_again:
+      break
 
-print("Gracias por jugar. ¡Hasta luego!")
+  print("Gracias por jugar. ¡Hasta luego!")
