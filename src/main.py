1fbde8fdfed5de2c992818ef5708aa94b55a2a5b
Sue419 <suzan419@gmail.com>
2024-02-28 23:16:06 -0500

    FEAT: Add get_user_guess function, implement colorama, and time.sleep

--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
+"""
+Modulo MAIN contiene funciones para el juego GUESS THE NUMBER.
+"""
 import random
+import time
+from colorama import Fore, Style, Back
 
+#FUNCIONES PARA EL JUEGO GUESS THE NUMBER
 def generate_random_number():
     return random.randint(1, 100)
 
+def get_user_guess():
+    while True:
+        try:
+            user_input = int(input('Ingrese un número entre 1 y 100: '))
+            if user_input < 1 or user_input > 100:
+                print("Por favor, ingrese un número válido entre 1 y 100.")
+            else:
+                return user_input
+        except ValueError:
+            print("Por favor, ingrese un número válido.")
+
 def human_guess(player_number, random_number):
     if player_number == random_number:
-        return '¡Número correcto! Adivinaste el número', True
+        return f'¡Número correcto! Adivinaste el número', True
     elif player_number > random_number:
         return 'Tu número es muy alto.', False
     else:
@@ -13,54 +30,64 @@ def human_guess(player_number, random_number):
 
 def computer_guess(computer_number, random_number):
     if computer_number == random_number:
-        return f'El ordenador adivina: {computer_number}. ¡Número correcto! La computadora adivinó el número', True
+        return f'El ordenador adivina: {computer_number}.\n¡Número correcto! La computadora adivinó el número', True
     elif computer_number > random_number:
-        return f'El ordenador adivina: {computer_number}. El número es muy alto', False
+        return f'El ordenador adivina: {computer_number}.\nEl número es muy alto', False
     else:
-        return f'El ordenador adivina: {computer_number}. El número es muy bajo', False
+        return f'El ordenador adivina: {computer_number}.\nEl número es muy bajo', False
 
+def record_guess(guess, guess_list):
+    guess_list.append(guess)
+
+#LOGICA PARA EL JUEGO GUESS THE NUMBER
 def guess_the_number():
 
-    random_number = generate_random_number()
-    print("¡Adivina el número!")
+    """
+    Esta función ejecuta la lógica principal del juego. Genera un número aleatorio entre 1 y 100, 
+    solicita al usuario que ingrese suposiciones, proporciona pistas sobre 
+    si la suposición es demasiado alta o demasiado baja, y al final imprime los
+    resultados del juego con los números que supuso el jugador y el computador.
+    Retorna:
+    bool: True si el jugador decide jugar de nuevo, False de lo contrario.
+    """
 
-    player_guesses =[]
+    random_number = generate_random_number()
+    print(f'\n {Fore.LIGHTBLUE_EX}{Style.BRIGHT}|------ ¡ADIVINA EL NÚMERO! -----|{Style.RESET_ALL}\n')
+
+    player_guesses = []
     computer_guesses = []
 
     while True:
-        player_number = int(input('Ingrese un número: '))
+        time.sleep(1)
+        player_number = get_user_guess()
         player_message, player_success = human_guess(player_number, random_number)
-        print(player_message)
-
-        player_guesses.append(player_number)
+        print(f'{player_message}\n')
+        record_guess(player_number, player_guesses)
 
         if player_success:
             break
 
+        time.sleep(1)
+
         computer_number = random.randint(1, 100)
         computer_message, computer_success = computer_guess(computer_number, random_number)
-        print(computer_message)
-
-        computer_guesses.append(computer_number)
+        print(f'{computer_message}\n')
+        record_guess(computer_number, computer_guesses)
 
         if computer_success:
             break
-    
-    # Lista de números de los intentos
-    print(f"Suposiciones del jugador: {player_guesses}")
-    print(f"Suposiciones del computador: {computer_guesses}")
 
-    play_again = input('¿Quieres jugar de nuevo? (si/no)? ')
+    print(f'\n{Fore.CYAN}{Style.BRIGHT}*** Resultados ***{Style.RESET_ALL}')
+    print(f'{Style.DIM}Suposiciones del jugador: {player_guesses}')
+    print(f'Suposiciones del computador: {computer_guesses}{Style.RESET_ALL}')
+
+    play_again = input('\n¿Quieres jugar de nuevo? (si/no)? ')
     return play_again.lower() == 'si'
 
-#Para ejecutar el juego
 if __name__ == "__main__":
-  while True:
-    # Jugar una partida
-    play_again = guess_the_number()
+    while True:
+        question_play_again = guess_the_number()
+        if not question_play_again:
+            break
 
-    # Salir del bucle si el usuario no quiere jugar de nuevo
-    if not play_again:
-      break
-
-  print("Gracias por jugar. ¡Hasta luego!")
+    print(Style.BRIGHT +'\nGracias por jugar. ¡Hasta luego!\n' + Style.RESET_ALL )
--- src/test.py
+++ src/test.py
@@ -1,16 +1,27 @@
+"""
+Modulo TEST contiene los test hechos a las funciones:
+generate_random_number, human_guess, computer_guess, get_user_guess
+"""
 import unittest
-from main import generate_random_number, human_guess, computer_guess
+from unittest.mock import patch
+from main import generate_random_number, human_guess, computer_guess, get_user_guess
 
 class TestGuessTheNumber(unittest.TestCase):
+    """Clase para realizar pruebas unitarias de las funciones del juego GUESS THE NUMBER."""
 
-    # Test para número aleatorio 
     def test_generate_random_number(self):
+        """Test para número aleatorio que verifica que el número sea entre 1 y 100"""
         random_number = generate_random_number()
 
-        # Verifica que el número sea entre 1 y 100
         self.assertGreaterEqual(random_number, 1)
         self.assertLessEqual(random_number, 100)
 
+    @patch('builtins.input', side_effect=['50'])
+    def test_get_user_guess_valid_input(self, mock_input):
+        """Test para la función GET USER GUESS, que verifica que el número ingresado esté en el rango del valor del juego"""
+        self.assertEqual(get_user_guess(), 50)
+
+    #TEST de la función human_guess
     def test_human_guess(self):
         player_number = 20
         random_number = 20
@@ -37,19 +48,19 @@ class TestGuessTheNumber(unittest.TestCase):
         random_number = 40
         computer_number = 40
         result, success = computer_guess(computer_number, random_number)
-        self.assertEqual(result, "El ordenador adivina: 40. ¡Número correcto! La computadora adivinó el número")
+        self.assertEqual(result, "El ordenador adivina: 40.\n¡Número correcto! La computadora adivinó el número")
         self.assertTrue(success)
 
         random_number = 40
         computer_number = 50
         result, success = computer_guess(computer_number, random_number)
-        self.assertEqual(result, "El ordenador adivina: 50. El número es muy alto")
+        self.assertEqual(result, "El ordenador adivina: 50.\nEl número es muy alto")
         self.assertFalse(success)
 
         random_number = 40
         computer_number = 30
         result, success = computer_guess(computer_number, random_number)
-        self.assertEqual(result, "El ordenador adivina: 30. El número es muy bajo")
+        self.assertEqual(result, "El ordenador adivina: 30.\nEl número es muy bajo")
         self.assertFalse(success)
 
 if __name__ == "__main__":
